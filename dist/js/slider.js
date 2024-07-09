c0a972703d749ccd6ee449421dc636cc9f714659
KenSumang <vistapaulrainier@gmail.com>
2024-07-09 15:55:04 +0800

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: ".aboutSlider__content",
+    items: 1,
+    speed:700,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+});
